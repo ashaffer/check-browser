4630ccbdbc4d06d3adf476f35ac69a17d72ce0de
Andrew Shaffer <andrew@weo.io>
2015-03-03 15:18:21 -0800

    fix brokenness

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var bowser = require('bowser');
+var bowser = require('bowser').browser;
 
 module.exports = function(supports) {
   var listed = false;
   return Object.keys(supports).some(function(name) {
     name = name.toLowerCase();
-    
+
     if(name !== 'others' && bowser[name]) {
       listed = true;
       return bowser.version >= supports[name];
--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 var mock = require('mock-require');
 var bowser = {};
-mock('bowser', bowser);
+mock('bowser', {browser: bowser});
 
 var expect = require('chai').expect;
 var supports = require('../');
@@ -9,17 +9,17 @@ describe('check-browser', function() {
   it('should work', function() {
     bowser.chrome = true;
     bowser.version = 33;
-    
+
     expect(supports({chrome: 33})).to.equal(true);
-    
+
     bowser.version = 32;
     expect(supports({chrome: 33})).to.equal(false);
   });
-  
+
   it('should allow toggling of whitelist/blacklist behavior with .others', function() {
     bowser.chrome = true;
     bowser.version = 33;
-    
+
     expect(supports({chrome: 34, others: true})).to.equal(false);
     expect(supports({firefox: 3, others: true})).to.equal(true);
   });
